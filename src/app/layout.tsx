cfb48d2896d47056a93df8caaa12286cc5c24088
Gildemberg Leite <gildembergleite@gmail.com>
2024-04-21 06:51:44 -0300

    lint: corrections

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ReactNode } from 'react'
 import './globals.css'
 
 const inter = Inter({
@@ -14,11 +15,7 @@ export const metadata: Metadata = {
   },
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html className={inter.variable} lang="pt">
       <body className="bg-zinc-950 text-zinc-50 antialiased">{children}</body>
